10957306b4b0ef6279373279531c9ba44ce7e562
conneralegre <conner.alegre@gmail.com>
2015-08-18 10:04:16 -0600

    Formatting changes

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
 var app = angular.module('redditClone', []);
 
-app.controller('MainCtrl', ['$scope', function ($scope) {
-  $scope.posts = [];
+app.controller('MainCtrl', ['$scope', function($scope) {
+    $scope.posts = [];
 
-  $scope.addPost = function(){
-    if(!$scope.title || $scope.title === '') { return; }
-    $scope.posts.push({
-      title: $scope.title,
-      link: $scope.link,
-      upvotes: 0
-    });
-    $scope.title = '';
-    $scope.link = '';
-  };
+    $scope.addPost = function() {
+        if (!$scope.title || $scope.title === '') {
+            return;
+        }
+        $scope.posts.push({
+            title: $scope.title,
+            link: $scope.link,
+            upvotes: 0
+        });
+        $scope.title = '';
+        $scope.link = '';
+    };
 
-  $scope.incrementVotes = function(post) {
-    var upArrow = document.getElementsByClassName('arrow')[0];
-    var downArrow = document.getElementsByClassName('arrow')[1];
-    upArrow.innerHTML = "&#9650;";
-    downArrow.innerHTML = "&#9661;";
-    post.upvotes += 1;
-  };
+    $scope.incrementVotes = function(post) {
+        var upArrow = document.getElementsByClassName('arrow')[0];
+        var downArrow = document.getElementsByClassName('arrow')[1];
+        upArrow.innerHTML = "&#9650;";
+        downArrow.innerHTML = "&#9661;";
+        post.upvotes += 1;
+    };
 
-  $scope.decrementVotes = function(post) {
-    var upArrow = document.getElementsByClassName('arrow')[0];
-    var downArrow = document.getElementsByClassName('arrow')[1];
-    downArrow.innerHTML = "&#9660;";
-    upArrow.innerHTML = "&#9651;";
+    $scope.decrementVotes = function(post) {
+        var upArrow = document.getElementsByClassName('arrow')[0];
+        var downArrow = document.getElementsByClassName('arrow')[1];
+        downArrow.innerHTML = "&#9660;";
+        upArrow.innerHTML = "&#9651;";
 
-    post.upvotes -= 1;
-  }
+        post.upvotes -= 1;
+    }
 }]);
